db609f1598a2e741d89a20df395478779cebf791
Messium <danielholstrom@gmail.com>
2025-10-15 22:11:53 +0200

    fix(recent_star): better path var

--- get_recent_stars_gh.sh
+++ get_recent_stars_gh.sh
@@ -2,8 +2,9 @@ star=$(gh api user/starred | jq -r '.[].full_name' | fzf)
 url=$(gh api repos/$star | jq -r '.html_url')
 echo "$url"
 firefox "$url"
+dest_path="$HOME/git"
 repo_name=$(echo "$url" | awk -F'/' '{print $NF}')
 response=$(gum choose "yes" "no" --header "Do you want to clone repo?")
 if [ "$response" == "yes" ]; then
-    git clone "$url" ~/git/"$repo_name"
+    git clone "$url" "$dest_path"/"$repo_name"
 fi
